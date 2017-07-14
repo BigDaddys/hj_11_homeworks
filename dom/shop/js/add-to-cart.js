d8e48a6b49a824e34fc471a91f7239eeaf227d81
Александр <shurik.box@gmail.com>
2017-07-14 21:48:19 +0300

    fix dom/shop/js/add-to-cart

--- dom/shop/js/add-to-cart.js
+++ dom/shop/js/add-to-cart.js
@@ -5,16 +5,16 @@ const cartTotalPrice = document.getElementById('cart-total-price');
 window.addEventListener('load', () => {
   let totalPrice = 0;
   let totalItems = 0;
+
+  function addToCart(event) {
+    totalItems++;
+    totalPrice += parseInt(event.currentTarget.dataset.price);
+
+    cartCount.innerHTML = totalItems;
+    cartTotalPrice.innerHTML = getPriceFormatted(totalPrice);
+  }
+
+  for (let btn of addBtn) {
+    btn.addEventListener('click', addToCart);
+  }
 });
-
-function addToCart(event) {
-  totalItems++;
-  totalPrice += parseInt(event.currentTarget.dataset.price);
-
-  cartCount.innerHTML = totalItems;
-  cartTotalPrice.innerHTML = getPriceFormatted(totalPrice);
-}
-
-for (let btn of addBtn) {
-  btn.addEventListener('click', addToCart);
-}
